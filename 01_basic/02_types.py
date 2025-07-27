ef893f8155576da165a797b5f8559edfece08627
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-07-26 22:37:49 -0400

    Data types
    Data types

---
+++ 01_basic/02_types.py
@@ -0,0 +1,56 @@
+'''
+    this is a comment
+'''
+# this is another comment
+
+print("int:")
+print(5)
+print(12)
+print(2014)
+print(-2)
+
+print(type(5))
+print(type(12))
+print(type(2014))
+print(type(-2))
+
+print("Float:")
+print(5.00)
+print(12.00)
+print(2014.00)
+print(-2.00)
+print(1e3)
+
+print(type(5.00))
+print(type(12.00))
+print(type(2014.00))
+print(type(-2.00))
+print(type(1e3))
+
+print("Complex:")
+print(1 + 2j)
+print(type(1 + 2j))
+
+print("string:")
+print("Hello")
+print("123")
+print("")
+print("""
+this is a multiline
+""")
+print(type("Hello"))
+print(type("123"))
+print(type(""))
+print(type("""
+this is a multiline
+"""))
+
+print("Bool:")
+print(True)
+print(False)
+print(type(True))
+print(type(False))
+
+print("NoneType")
+print(None)
+print(type(None))
