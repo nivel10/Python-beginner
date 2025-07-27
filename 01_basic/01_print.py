4b4a436d49efb538ecdd7998231e9d8871108c7f
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-07-26 22:11:47 -0400

    Using the print function
    Using the print function

---
+++ 01_basic/01_print.py
@@ -0,0 +1,9 @@
+print("hello, worl...!!!")
+print('hello, worl (same)...!!!')
+
+print("Python", "is", "great")
+print("Python", "is", "great", sep = "-")
+print("This printed", end = " ")
+print("on one line")
+
+print(2014)
