c4c7397ece47768572c11991297baaa617a6c3c2
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-07-29 16:22:34 -0400

    challenge find first sum
    challenge find first sum

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -4,3 +4,27 @@ os.system('cls')
 
 # --------------------------------------------
 print('')
+
+def find_first_sum(nums: list[int | float], goal: int | float) -> list[int | float]:
+
+    nums_clone: list[int | float] = nums[:]
+    index: int = 0
+
+    for index_1 in range(len(nums)):
+        for index_2 in range(index_1 + 1, len(nums)):
+            if nums[index_1] + nums[index_2] == goal:
+                return [index_1, index_2]
+            
+    return None
+
+nums: list[int | float] = [0, 2, 2, 3, 5, 6]
+# nums: list[int | float] = [0, 2, 2, 3, 5, 6]
+goal: int | float = 8
+
+result: int | float = 0
+result = find_first_sum(nums=nums, goal=goal)
+print(result)
+
+print('')
+result = find_first_sum(nums=nums, goal=19)
+print(result)
