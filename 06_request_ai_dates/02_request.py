a790c8f1501e864fb28ef840110931b4f73a1caa
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-07-31 16:34:11 -0400

    request
    request

--- 06_request_ai_dates/02_request.py
+++ 06_request_ai_dates/02_request.py
@@ -1,3 +1,20 @@
 import os
 # os.system('clear')
 os.system('cls')
+
+# ---------------------------------------
+print('')
+import urllib.request
+import json
+
+try:
+    api_url: str = 'https://jsonplaceholder.typicode.com/posts/'
+    response = urllib.request.urlopen(api_url)
+    data = response.read()
+    data_json = json.loads(data.decode('utf-8'))
+
+    print(data_json)
+
+    response.close()
+except urllib.error.URLError as e:
+    print('Error: {e}')
