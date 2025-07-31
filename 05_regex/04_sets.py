83c1fa8e2d80f05b59bf86845379a5a28de0e041
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-07-31 12:30:08 -0400

    sets
    sets

--- 05_regex/04_sets.py
+++ 05_regex/04_sets.py
@@ -6,3 +6,30 @@ import re
 
 # --------------------------------------------
 print('')
+# text: str = 'ni.vel_10'
+text: str = 'ni.v#l_10'
+# pattern: str = r'[\w._%+-]'
+pattern: str = r'^[\w._%+-]+$'
+
+result = re.search(pattern=pattern, string=text)
+
+if result:
+    print('The user name is valid.')
+else:
+    print('The user name is not valid.')
+
+# --------------------------------------------
+print('')
+text = 'Hello world'
+pattern = r'[aeiou]'
+result_all: list[any] = re.findall(pattern=pattern, string=text)
+
+print(result_all)
+
+# --------------------------------------------
+print('')
+text = 'man ban ran fan tan ram'
+pattern = r'[fmr]an'
+result_all = re.findall(pattern=pattern, string=text)
+
+print(result_all)
