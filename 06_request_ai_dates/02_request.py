87be3617917c1b17520538003b3f3be62de7da82
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-01 19:12:13 -0400

    ai
    ai

--- 06_request_ai_dates/02_request.py
+++ 06_request_ai_dates/02_request.py
@@ -11,8 +11,18 @@ print('')
 import urllib.request
 import json
 
+api_url: str = ''
+api_headers: object = {}
+data: any = {}
+status_code: int = 0
+data_json: any = ''
+data_edit: object = {}
+response: any = {}
+result: object = {}
+ai_response: str = ''
+
 try:
-    api_url: str = 'https://jsonplaceholder.typicode.com/posts/'
+    api_url = 'https://jsonplaceholder.typicode.com/posts/'
     response = urllib.request.urlopen(api_url)
     data = response.read()
     data_json = json.loads(data.decode('utf-8'))
@@ -39,7 +49,7 @@ print(data[0])
 try:
     print('')
     api_url_error = f'https://jsonplaceholder.typecode.c'
-    data_edit: object = {
+    data_edit = {
         'userId': 5,
         'title' : 'nivel',
         'body': '10'
@@ -48,7 +58,7 @@ try:
     response = requests.post(url=api_url, json=data_edit)
     # response = requests.post(url=api_url_error, json=post_data)
     data = response.json()
-    status_code: int = response.status_code
+    status_code = response.status_code
     print(f'result: {data}\nstatus code: {status_code}')
 except requests.exceptions.RequestException as e:
     print(f'Error: {e}')
@@ -93,8 +103,9 @@ except requests.exceptions.RequestException as e:
 
 
 # -------------------------------------------
-def ai_geminis(api_key: str = '', prompt: str = '') -> object:
-    result: object = {
+print('')
+def ai_deepmind(api_key: str = '', prompt: str = '') -> object:
+    result= {
         'status_code': 200,
         'result': {},
     }
@@ -161,10 +172,10 @@ def ai_geminis(api_key: str = '', prompt: str = '') -> object:
 
 
 result_str: str = ''
-ai_response: str = ''
-ai_apy_key = os.environ.get('AI_GEMINIS_API')
-result: object = ai_geminis(
-    api_key=ai_apy_key, 
+# ai_response: str = ''
+ai_deepmind_api: str = os.environ.get('AI_GEMINIS_API')
+result= ai_deepmind(
+    api_key=ai_deepmind_api, 
     prompt='Hola, por favor hazme un poema super corto sobre python.'
 )
 
@@ -183,3 +194,51 @@ if result:
         # print(f'status code: {result['status_code']}')
         # print(json.dumps(obj=result['result'], indent=2))
         print(result_str)
+
+# -------------------------------------------
+print('')
+def ai_deepseek(api_key: str = '', model: str = '', prompt: str = '') -> object:
+    result= {'status_code': 200, 'result': {}} 
+    try:
+        api_url = f'https://api.deepseek.com/chat/completions'
+        api_headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {api_key}'
+        }
+
+        data = {
+            # 'model': f'{model}:free',
+            'model': model,
+            'messages': [
+                {'role': 'user', 'content': prompt,},
+            ],
+            'stream': False,
+        }
+
+        response = requests.post(url=api_url, json=data, headers=api_headers)
+
+        result['result'] = response.json()
+        return result
+    except Exception as e:
+        result['status_code'] = 400
+        result['result'] = e
+        return result
+
+ai_deepseek_api: str = os.environ.get('AI_DEEPSEEK_API')
+result = ai_deepseek(
+    api_key=ai_deepseek_api, 
+    model='deepseek-chat',
+    # model='deepseek/deepseek-chat:free',
+    prompt='Hola, por favor hazme un poema corto de python. Gracias'
+)
+
+if result:
+    if result['status_code'] == 200:
+        if 'error' in result['result']:
+            ai_response = json.dumps(obj=result['result']['error'], indent=2)
+            print(f'{ai_response}')
+        else:
+            ai_response = json.dumps(obj=result['result'], indent=2)
+            print(ai_response)
+    else:
+        print(json.dumps(obj=result['result'], indent=2))
