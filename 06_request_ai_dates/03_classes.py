3f0111baa7b8794ffdb3cd992acbda25564cb2a1
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-01 21:34:22 -0400

    scraping
    scraping

--- 06_request_ai_dates/03_classes.py
+++ 06_request_ai_dates/03_classes.py
@@ -1,3 +1,18 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+match os.name:
+    case 'nt':
+        os.system('cls')
+    case 'posix':
+        os.system('clear')
+    case 'java':
+        print(os.name)
+
 class Vehicle:
 
     # class atribute
@@ -27,3 +42,114 @@ print('')
 my_auto_2.start_vehicle()
 print(f'Mark: {my_auto_2.mark}')
 print(my_auto_2.type)
+
+
+# --------------------------------------------
+print('')
+class AI_API:
+    def __init__(self, api_key: str = '', api_url: str = '', model: str = '', type: int = 0, ):
+        self.api_key = api_key
+        self.api_url = api_url
+        self.model = model
+        self.type = type
+
+    def call(self, promt: str = '') -> object:
+        result: object = {'status_code': int, 'result': object}
+        result['status_code'] = 200
+        try:
+            api_headers: object = {
+                'Content-Type': 'application/json',
+            }
+            api_data: object = {}
+
+            match self.type:
+                case 0:
+                    api_headers['x-goog-api-key'] = self.api_key
+                    api_data: object = {
+                        'contents': [
+                            {
+                                'parts': [
+                                    {
+                                        'text': promt,
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                case 1:
+                    api_headers['Authorization'] = f'Bearer {self.api_key}'
+                    api_data: object = {
+                        'model': self.model,
+                        'messages': [
+                            {'role': 'user', 'content': promt, }
+                        ],
+                        'stream': False
+                    }
+
+            response: object = requests.post(
+                url=self.api_url,
+                json=api_data,
+                headers=api_headers
+            )
+
+            result['result'] = response.json()
+            return result
+        except Exception as ex:
+            result['status_code'] = 400
+            result['result'] = ex
+            return result
+
+# -------------------------------------------------------
+print('')
+deepmind = AI_API(
+    api_key=os.environ.get('AI_GEMINIS_API'),
+    api_url='https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
+    model='',
+    type=0
+)
+
+prompt: str = 'Hola, por favor hazme un poema corto sobre la musica. Gracias'
+result_str: str = ''
+result: object = {'status_code': int, 'result': object}
+result = deepmind.call(promt=prompt)
+
+if result:
+    if result['status_code'] == 200:
+        # # print(result)
+        # result_str = json.dumps(obj=result['result'], indent=2)
+        # print(result_str)
+        print(result['result']['candidates'][0]['content']['parts'][0]['text'])
+        print('')
+    else:
+        result_str = f'status code: {result['status_code']}\n'
+        # # result_str += f'result: {json.dumps(result['result'], indent=2)}\n'
+        # result_str += f'result: {json.dumps(result['result'], indent=2)}\n'
+        result_str += f'result: {result['result']}\n'
+        print(result_str)
+else:
+    print('The process is not executed')
+
+# ------------------------------------------
+print('')
+deepseek = AI_API(
+    api_key=os.environ.get('AI_DEEPSEEK_API'),
+    api_url='https://api.deepseek.com/chat/completions',
+    model='deepseek-chat',
+    type=1,
+)
+
+result = deepseek.call(promt=prompt)
+
+if result:
+    if result['status_code'] == 200:
+        if 'error' in result['result']:
+            result_str = json.dumps(obj=result, indent=2)
+            print(result_str)
+        else:
+            print(result)
+    else:
+        # print(result)
+        result_str = json.dumps(obj=result, indent=2)
+        print(result_str)
+else: 
+    print('The process is not executed')
