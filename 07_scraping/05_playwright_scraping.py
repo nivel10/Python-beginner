bc6581e5f8d1e63fa85a87790e53c4ef4908185e
Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-02 13:46:02 -0400

    playwright
    playwright

--- 07_scraping/05_playwright_scraping.py
+++ 07_scraping/05_playwright_scraping.py
@@ -1,6 +1,5 @@
 import os
-import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import sync_playwright
 
 match os.name:
     case 'nt':
@@ -12,3 +11,23 @@ match os.name:
 
 # -----------------------------------------------------
 print('')
+
+url: str = 'https://midu.dev/'
+
+with sync_playwright() as p:
+    # browser = p.chromium.launch(headless=False, slow_mo=2000)
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(url)
+
+    first_article_anchor = page.locator('article a').first
+    first_article_anchor.click()
+
+    page.wait_for_load_state()
+
+    first_image = page.locator('main img').first
+    # print(first_image.get)
+    image_src = first_image.get_attribute('src')
+    print(f'The image of the first curso is this: {image_src}')
+
+    browser.close()
